3eb7b1099a6159a6ee9214b1407fa3c3574ca4b0
Qu1et-x <lum27149@gmail.com>
2025-08-25 16:14:42 +0800

    finished hw01b

--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,29 +1,64 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExercises {
 
+    private static final Logger log = LoggerFactory.getLogger(ListExercises.class);
+
     /** Returns the total sum in a list of integers */
     public static int sum(List<Integer> L) {
         // TODO: Fill in this function.
-        return 0;
+        int total = 0;
+        for (int i : L) {
+            total += i;
+        }
+        return total;
     }
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
         // TODO: Fill in this function.
-        return null;
+        List<Integer> lst = new ArrayList<>();
+        for (int i : L) {
+            if (i % 2 == 0) {
+                lst.add(i);
+            }
+        }
+        return lst;
     }
 
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
         // TODO: Fill in this function.
-        return null;
+        List<Integer> lst = new ArrayList<>();
+        if (L1 == null || L2 == null) return lst;
+        for (int i : L1) {
+            for (int j : L2) {
+                if (!(lst.contains(i)) && i == j) {
+                    lst.add(i);
+                    break;
+                }
+            }
+        }
+        return lst;
     }
 
 
     /** Returns the number of occurrences of the given character in a list of strings. */
     public static int countOccurrencesOfC(List<String> words, char c) {
         // TODO: Fill in this function.
-        return 0;
+        int times = 0;
+        for (String i : words) {
+            if (i == null) continue;
+            for (int j = 0; j < i.length(); j++) {
+                if (i.charAt(j) == c) {
+                    times++;
+                }
+            }
+        }
+        return times;
     }
 }
--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,3 +1,6 @@
+import edu.princeton.cs.algs4.In;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,7 +10,11 @@ public class MapExercises {
      */
     public static Map<Character, Integer> letterToNum() {
         // TODO: Fill in this function.
-        return null;
+        Map<Character, Integer> map = new HashMap<>();
+        for (char next = 'a'; next <= 'z'; next++) {
+            map.put(next, next - 'a' + 1);
+        }
+        return map;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
@@ -15,12 +22,20 @@ public class MapExercises {
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
         // TODO: Fill in this function.
-        return null;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            map.put(i, i * i);
+        }
+        return map;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
         // TODO: Fill in this function.
-        return null;
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        return map;
     }
 }
